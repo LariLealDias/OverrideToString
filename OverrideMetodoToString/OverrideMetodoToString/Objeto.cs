a4da9c6464d885b1c5f87f4299bc5567e8407d2d
Lari <larileal@hotmail.com.br>
2023-02-02 12:20:07 -0300

    pratica

---
+++ OverrideMetodoToString/OverrideMetodoToString/Objeto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace OverrideMetodoToString
+{
+    public class Objeto
+    {
+        public string Name { get; set; }
+        public string Idade { get; set; }
+        public int Classificacao { get; set; }
+
+        ////Se aceitar o default, a saida será default
+        //public override string ToString()
+        //{
+        //    return base.ToString();
+        //}
+
+        //Pre formatando
+        public override string ToString()
+        {
+            return $"Nome de usuário: {this.Name} | Pontos: {this.Classificacao}";
+        }
+    }
+}
---
+++ OverrideMetodoToString/OverrideMetodoToString/Program.cs
@@ -0,0 +1,17 @@
+﻿using OverrideMetodoToString;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Objeto objeto1 = new Objeto();
+            objeto1.Name = "Fulano";
+            objeto1.Classificacao = 10;
+
+            Console.WriteLine(objeto1);
+
+        }
+    }
+}
